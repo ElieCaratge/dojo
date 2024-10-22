c9a02af57a62e8a75238faffd1bcea5dc6e85c82
Elie Caratgé <ecaratge@gmail.com>
2024-10-22 14:00:22 +0200

    Clean before TP2

--- src/utils/send-data.ts
+++
@@ -1,8 +0,0 @@
-export const sendData = (kcal: number) => () => {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ kcal: kcal }),
-  };
-  fetch(`.netlify/functions/fauna`, requestOptions);
-};
